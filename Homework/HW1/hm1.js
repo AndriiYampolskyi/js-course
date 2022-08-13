7730e378890d80f2eea7b60e264535e00ed620c7
AndriiYampolskyi <andrii.yampolskyi.js@gmail.com>
2022-08-13 21:29:47 +0300

    lesson1

---
+++ Homework/HW1/hm1.js
@@ -0,0 +1,44 @@
+// 1 задача
+
+/*const userName = "Andrii";
+const surName = " Yampolskyi";
+prompt("Your first and last name?");
+alert(userName + surName);
+
+let userName = "Andrii";
+userName += " Yampolskyi";
+prompt("Your first and last name?");
+alert(userName);*/
+
+//2 задача
+
+/*alert(2 ** 5);
+
+Math.pow(2, 5);*/
+
+//3 задача
+
+/*const x = 3.456;
+const y = 6.453;
+const square = x * y;
+alert(Math.floor(square * 100) / 100);*/
+
+// 4 задача
+/*let age = prompt("Введите Ваш возраст!", 18);
+if (age >= 18) {
+  alert("Ваш возраст соответсвует требованию");
+} else {
+  alert("Вам отказано в доступе");
+}*/
+
+// 5 задача
+/*let yourName = prompt("Привет,Коля,Андрей,Федор??", "");
+alert(`Привет ${yourName}`); */
+
+//6 задача
+/*let mystery = prompt("Волосатая головка за щеку заходит ловко,что это?");
+if (mystery === "Щетка" || mystery === "щетка") {
+  alert("Верно!");
+} else {
+  alert("Попробуй снова!");
+}*/
